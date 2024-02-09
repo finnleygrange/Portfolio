0ce085ceaf4e1871d23c410661b5e506fdb0f02a
Finnley Grange <dev.finnleygrange@icloud.com>
2024-02-09 00:37:54 +0000

    Open links in a new tab

--- client/src/components/SocialLinks.jsx
+++ client/src/components/SocialLinks.jsx
@@ -3,19 +3,24 @@ import React from "react";
 function SocialLinks() {
   return (
     <div className="icons d-flex justify-content-center align-items-center">
-      <a href="https://github.com/finnleygrange" title="Github">
+      <a href="https://github.com/finnleygrange" title="Github" target="_blank">
         <i className="icon fa-brands fa-github"></i>
       </a>
-      <a href="https://www.linkedin.com/in/finnleygrange/" title="LinkedIn">
+      <a
+        href="https://www.linkedin.com/in/finnleygrange/"
+        title="LinkedIn"
+        target="_blank"
+      >
         <i className="icon fa-brands fa-linkedin"></i>
       </a>
       <a
         href="https://www.youtube.com/channel/UC4qtyutKxBjZu8Od91uaAjg"
         title="YouTube"
+        target="_blank"
       >
         <i className="icon fa-brands fa-youtube"></i>
       </a>
-      <a href="">
+      <a href="" target="_blank">
         <i className="icon fas fa-file-alt resume-icon" title="Resume"></i>
       </a>
     </div>
